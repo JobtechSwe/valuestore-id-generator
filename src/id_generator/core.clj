61a22372593128f43528e8cf445c2f9d85362439
SickanEkman <sara.d.ekman@gmail.com>
2018-11-21 18:43:29 +0100

    + Docstrings

--- src/id_generator/core.clj
+++ src/id_generator/core.clj
@@ -7,22 +7,24 @@
 (def base-59-nano-id (generate "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ_"))
 
 (defn generate-new-id []
+  "Specify format and length of nano ID"
   (base-59-nano-id 8)
   )
 
 (defn get-ids []
+  "Get specific number of IDs to generate"
   (set (take 30000 (repeatedly generate-new-id)))
   )
 
-
 (defn write-ids-to-file! [ids]
+  "write IDs to file 'ids.txt', one per line"
   (doseq [id ids]
     (spit "ids.txt" (str id "\n") :append true)
     )
   )
 
 (defn -main
-  "Writing random ids to file"
+  "Write random nano IDs and save to file"
   [& args]
   (write-ids-to-file! (get-ids))
   )
