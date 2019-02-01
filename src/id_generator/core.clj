9af7fcc3b9b9a8c725ac76661a0d4637d15e3ede
qeshi <6067421+qeshi@users.noreply.github.com>
2019-02-01 17:10:47 +0100

    added underscore into id

--- src/id_generator/core.clj
+++ src/id_generator/core.clj
@@ -4,12 +4,21 @@
     [nano-id.custom :refer [generate]])
   )
 
-(def base-59-nano-id (generate "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ_"))
+(def base-59-nano-id (generate "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"))
 
 (defn generate-new-id []
   "Specify format and length of nano ID"
   (base-59-nano-id 10)
   )
+
+(defn add-underscore [id]
+  (str (subs id 0 4) "_" (subs id 4 7)  "_" (subs id 7 10)  )
+  )
+
+(defn generate-new-id-with-underscore []
+  (add-underscore (generate-new-id))
+  )
+
 
 (defn get-ids []
   "Get specific number of IDs to generate"
@@ -28,3 +37,5 @@
   [& args]
   (write-ids-to-file! (get-ids))
   )
+
+(def say-hello (fn [] (print "hello!")))
